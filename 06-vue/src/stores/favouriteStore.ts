8cfe4fb7ee7bb2097b39849a792bfcd6bab06104
Unai Zorrilla Castro <unaizc@gmail.com>
2025-11-16 12:30:15 +0100

    Added pinia plugin persisted state

--- 06-vue/src/stores/favouriteStore.ts
+++ 06-vue/src/stores/favouriteStore.ts
@@ -1,23 +1,26 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useFavouriteStore = defineStore('useFavouriteStore', () => {
-  const meals = ref<string[]>([])
+export const useFavouriteStore = defineStore('useFavouriteStore', {
+  state: () => {
+    const meals = ref<string[]>([])
 
-  function addFavourite(meal: { name: string }) {
-    if (!meals.value.includes(meal.name)) {
-      meals.value.push(meal.name)
+    function addFavourite(meal: { name: string }) {
+      if (!meals.value.includes(meal.name)) {
+        meals.value.push(meal.name)
+      }
     }
-  }
 
-  function removeFromFavourite(meal: { name: string }) {
-    meals.value = meals.value.filter(m => m !== meal.name)
-  }
+    function removeFromFavourite(meal: { name: string }) {
+      meals.value = meals.value.filter(m => m !== meal.name)
+    }
 
-  function containsMeal(mealName: string) : boolean {
-    return meals.value.includes(mealName)
-  }
+    function containsMeal(mealName: string) : boolean {
+      return meals.value.includes(mealName)
+    }
 
-  return { meals, addFavourite, removeFromFavourite, containsMeal }
-})
+    return { meals, addFavourite, removeFromFavourite, containsMeal }
+  },
+ persist: true
+});
 
--- 06-vue/src/stores/weakMealStore.ts
+++ 06-vue/src/stores/weakMealStore.ts
@@ -1,25 +1,28 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useWeakMealStore = defineStore('useWeakMealStore', () => {
-  const meals = ref({
-    Sunday: [] as string[],
-    Monday: [] as string[],
-    Tuesday: [] as string[],
-    Wednesday: [] as string[],
-    Thursday: [] as string[],
-    Friday: [] as string[],
-    Saturday: [] as string[],
-  })
+export const useWeakMealStore = defineStore('useWeakMealStore', {
+  state: () => {
+    const meals = ref({
+      Sunday: [] as string[],
+      Monday: [] as string[],
+      Tuesday: [] as string[],
+      Wednesday: [] as string[],
+      Thursday: [] as string[],
+      Friday: [] as string[],
+      Saturday: [] as string[],
+    })
 
-  function addMeal(meal: { name: string; weekDay: keyof typeof meals.value }) {
-    meals.value[meal.weekDay].push(meal.name)
-  }
+    function addMeal(meal: { name: string; weekDay: keyof typeof meals.value }) {
+      meals.value[meal.weekDay].push(meal.name)
+    }
 
-  function deleteMeal(meal: { name: string; weekDay: keyof typeof meals.value }) {
-    meals.value[meal.weekDay] = meals.value[meal.weekDay].filter(m => m !== meal.name)
-  }
+    function deleteMeal(meal: { name: string; weekDay: keyof typeof meals.value }) {
+      meals.value[meal.weekDay] = meals.value[meal.weekDay].filter(m => m !== meal.name)
+    }
 
-  return { meals, addMeal, deleteMeal }
-})
+    return { meals, addMeal, deleteMeal }
+  },
+  persist: true
+});
 
